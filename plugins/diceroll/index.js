34d2ac295f81c867ecb4f1180a54e98eb51c32e5
Keanu73 <guestone63@gmail.com>
2019-09-18 19:06:11 +0100

    Refactored diceroll

--- plugins/diceroll/index.js
+++ plugins/diceroll/index.js
@@ -1,40 +1,28 @@
-var result;
-var dice;
-var sides = 6;
-var i;
-var total;
-var content;
-var numbers = [1,6];
-var roll;
-var sender;
 global.Brikkit.on('chat', evt => {
-		content = evt.getContent();
-		sender = evt.getSender().getUsername();
-		if(content.includes('!diceroll') & /\d/.test(content) == true) {
-			numbers = content.match(/\d+/g).map(Number);
-			result = 0;
-			dice = numbers[0];
-			sides = numbers[1];
-			if(dice > 100) {
-				dice = 100;
-			}
-			if(sides > 1000) {
-				sides = 1000;
-			}
-			if(isNaN(sides) == true) {
-				sides = 6
-			}
-			if(dice == 0) {
-				dice = 1
-			}
-			for (i = 0; i < dice;i++) {
-				roll = Math.floor(Math.random() * sides) + 1;
-				if(roll > sides) {
-					roll = sides
-				}
-				result += roll;
-			}
-			global.Brikkit.say(sender+' rolled '+dice+' d '+sides+' to get '+result);
-	}	
-	
+	const content = evt.getContent();
+	const sender = evt.getSender().getUsername();
+	if (content.includes('!roll') & /\d/.test(content) == true) {
+		let numbers = [1, 6];
+		let sides = 6;
+		let dice = numbers[0];
+		numbers = content.match(/\d+/g).map(Number);
+		sides = numbers[1];
+		let result = 0;
+
+		if (dice > 100) dice = 100;
+
+		if (sides > 1000) sides = 1000;
+
+		if (isNaN(sides)) sides = 6;
+
+		if (dice === 0) dice = 1;
+
+		for (let i = 0; i < dice; i++) {
+			let roll = Math.floor(Math.random() * sides) + 1;
+			if (roll > sides) roll = sides;
+			result += roll;
+		}
+
+		global.Brikkit.say(`${sender} rolled ${dice} d ${sides} to get ${result}`);
+	}
 });
