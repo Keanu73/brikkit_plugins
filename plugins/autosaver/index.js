b94837580db3f4f6fa0058bc5a414e62e7326cb8
Keanu73 <guestone63@gmail.com>
2019-09-18 19:04:52 +0100

    Added timestamp functionality to autosaver, refactored diceroll

--- plugins/autosaver/index.js
+++ plugins/autosaver/index.js
@@ -4,15 +4,15 @@ const confPath = `${process.cwd()}/conf/autosaver.json`;
 
 let conf;
 try {
-	conf = require(confPath);
-} catch(e) {
-	conf = {
-		intervalInSeconds: 300,
-		saveName: 'autosave_%date',
-		saveMessage: 'Autosaved bricks (if any).'
-	};
+    conf = require(confPath);
+} catch (e) {
+    conf = {
+        intervalInSeconds: 300,
+        saveName: 'autosave_%date',
+        saveMessage: 'Autosaved bricks (if any).'
+    };
 
-	fs.writeFileSync(confPath, JSON.stringify(conf, null, 2));
+    fs.writeFileSync(confPath, JSON.stringify(conf, null, 2));
 }
 
 function autosave() {
@@ -20,8 +20,10 @@ function autosave() {
     const isoDate = (new Date()).toISOString();
     const finalSaveName = conf.saveName.replace('%date', isoDate);
 
+    const timeStamp = `[${(new Date()).getHours()}:${(new Date()).getMinutes()}]`
+
     global.Brikkit.saveBricks(finalSaveName);
-    global.Brikkit.say(conf.saveMessage);
+    global.Brikkit.say(`${timeStamp} ${conf.saveMessage}`);
 }
 
 const dateRegex = `\\d\\d\\d\\d-\\d\\d-\\d\\dT\\d\\d_\\d\\d_\\d\\d\\.\\d\\d\\dZ`;
@@ -30,28 +32,28 @@ global.Brikkit.on('start', evt => {
         // create a regex for matching the autosave file (include %date)
         const regexString = `^${conf.saveName.replace('%date', dateRegex)}$`;
         const regex = new RegExp(regexString);
-        
+
         // find the latest file that corresponds to the regex
         let latestSave = undefined;
-        for(let i = 0; i < saves.length; ++i) {
+        for (let i = 0; i < saves.length; ++i) {
             const save = saves[i];
-            
-            if(!regex.test(save))
+
+            if (!regex.test(save))
                 continue;
-            
-            if(latestSave === undefined) {
+
+            if (latestSave === undefined) {
                 latestSave = save;
                 continue;
             }
-            
-            if(save > latestSave)
+
+            if (save > latestSave)
                 latestSave = save;
         }
-        
+
         // if a file was found, load it
-        if(latestSave !== undefined)
+        if (latestSave !== undefined)
             global.Brikkit.loadBricks(latestSave);
-        
+
         // start autosaving after we load the save
         setInterval(autosave, conf.intervalInSeconds * 1000);
     });
